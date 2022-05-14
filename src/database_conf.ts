028e0fc553b5914f26e66af9508c0e8c1c47bced
egoldshm <eitntt@gmail.com>
2022-05-14 23:34:44 +0300

    added process env

--- src/database_conf.ts
+++ src/database_conf.ts
@@ -1,7 +1,7 @@
 const database_conf = {
     host: 'localhost',
-    user: 'root',
-    password: 'SQL_Opora123',
+    user: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD,
     database: 'SQLOpora',
     insecureAuth: true
 };
