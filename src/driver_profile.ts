7086d1077f8aa8d6b991f959be53c79c6c24671c
egoldshm <eitntt@gmail.com>
2022-05-15 17:32:57 +0300

    Update driver_profile.ts

--- src/driver_profile.ts
+++ src/driver_profile.ts
@@ -5,7 +5,7 @@ import {Connection} from 'mysql2';
  * Get a specific driver by id with all of his races sorted by date from newest to the oldest with:
  * Average lap time, Fastest lap time, Slowest lap time, Number of pit stops, Fastest pit stop, Slowest pit stop, Circuit name, Points, Position
  * 
- * @param db the database connection
+ * @param {Connection} db the database connection
  * @param {string} driverId the driver id to get
  * @param {function} callback the callback function to call when the query is done
  */
@@ -20,7 +20,7 @@ import {Connection} from 'mysql2';
     circuits.\`name\` as 'circuit_name',
     sum(results.points) as sum_points, results.position
     from results, races, lap_times, pit_stops, circuits
-    where races.raceid = results.raceid and results.driverid = ${driverId}
+    where races.raceid = results.raceid and results.driverid = '${driverId}'
     and lap_times.raceId = races.raceid and lap_times.driverId = results.driverid
     and pit_stops.raceId = races.raceid and pit_stops.driverId = results.driverid
     and circuits.circuitid = races.circuitid
@@ -36,13 +36,13 @@ import {Connection} from 'mysql2';
   * Get a specific driver by id with all of his races sorted by date from newest to the oldest with:
   * Average lap time, Fastest lap time, Slowest lap time, Number of pit stops, Fastest pit stop, Slowest pit stop, Circuit name, Points, Position
   * 
-  * @param db the database connection
+  * @param {Connection} db the database connection
   * @param {string} driverName the driver's name to get the
   * @param {function} callback the callback function to call when the query is done
   */
   function driver_profile_by_name(db: Connection, driverName:string, callback: Function)
   {
-     let query = `select results.raceid, avg(lap_times.milliseconds) as 'average_lap_time',
+     const query = `select results.raceid, avg(lap_times.milliseconds) as 'average_lap_time',
      min(lap_times.milliseconds) as 'fastest_lap_time',
      max(lap_times.milliseconds) as 'slowest_lap_time',
      sum(pit_stops.\`stop\`) as 'number_of_pit_stops',
